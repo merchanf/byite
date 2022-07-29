0bfa9f25d969486fd2a64fce64fece17839169c4
Fabian Merchan <fabian@merchan.co>
2022-07-29 16:56:39 -0500

    removed default files from storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 const App = () => {
   const [count, setCount] = useState(0);
-
-  const obj = {
-    name: "John",
-    age: 30,
-  };
 
   return (
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
