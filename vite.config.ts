d23b4107180a6420edfb99aad5db2ab55102ec26
Fabian Merchan <fabian@merchan.co>
2022-08-01 23:22:15 -0500

    added colors and prefixed main stylesheet to all scss files

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-
-// https://vitejs.dev/config/
+const pathSrc = path.resolve(__dirname, "./src");
 export default defineConfig({
   plugins: [react()],
-  resolve:{
-    alias:{
-      '@' : path.resolve(__dirname, './src'),
-      '@components' : path.resolve(__dirname, './src/components'),
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "./src"),
+      },
+      {
+        find: "@components",
+        replacement: path.resolve(__dirname, "./src/components"),
+      },
+    ],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/main";`,
+      },
     },
   },
 });
