0c9c2b1a109b1924e96879dc4510e48b6a283cf2
Fabian Merchan <fabian@merchan.co>
2022-08-04 17:35:17 -0500

    Added global import for storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,4 +1,31 @@
+const { mergeConfig } = require('vite');
+const path = require("path");
+
 module.exports = {
+  async viteFinal(config, { configType }) {
+    // return the customized config
+    return mergeConfig(config, {
+      resolve: {
+        alias: [
+          {
+            find: "@",
+            replacement: path.resolve(__dirname, "./src"),
+          },
+          {
+            find: "@components",
+            replacement: path.resolve(__dirname, "./src/components"),
+          },
+        ],
+      },
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: `@import "./src/styles/main";`,
+          },
+        },
+      },
+    });
+  },
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
